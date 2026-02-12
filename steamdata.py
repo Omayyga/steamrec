b8d5ca54ba3a502285fbcae543fd403d9840cd67
sumi <81728206+Omayyga@users.noreply.github.com>
2026-02-12 23:10:09 +0000

    cache system in place for "appdetails" based on how recent data is.

--- steamdata.py
+++ steamdata.py
@@ -46,3 +46,30 @@ async def f_appdetails_store(appid: int) -> dict | None:
         return None
     
     return app_entry.get("data")
+
+# >>> basically ensures data is up to date. <<<
+async def f_appdetails_cached(appid: int, ttl_seconds: int = 60 * 60 * 24 * 7) -> dict | None: # >>> ttl_seconds -> cached data lifespan <<<
+
+    r = single_fetch("SELECT json, fetched_at FROM app_details WHERE appid = ?", [appid])
+    current_time = timestamp()
+
+    if r:
+        data_age = current_time - r["fetched_at"]
+        if data_age < ttl_seconds:
+            return json.loads(r["json"])
+
+    data = await f_appdetails_store(appid)
+    if data is None:
+        return None
+    
+    exec(
+        """
+        INSERT INTO app_details (appid, json, fetched_at)
+        VALUES (?, ?, ?)
+        ON CONFLICT(appid) DO UPDATE SET
+            json = excluded.json,
+            fetched_at = excluded.fetched_at
+        """,
+        [appid, json.dumps(data), current_time]
+    )
+    return data
