b74f3c84a568bbb46243314b6bd81ad1d805b26a
sumi <81728206+Omayyga@users.noreply.github.com>
2026-03-06 02:28:00 +0000

    Add category based profile generation; improve candidate selection logic

--- rec.py
+++ rec.py
@@ -1,5 +1,6 @@
 import math
 import json
+import random
 
 from collections import Counter
 from db import all_fetch
@@ -47,6 +48,40 @@ async def BuildUserProfile_genre(steamid64: str, TopGames_n: int = 50) -> Counte
 
             return profile
         
+async def BuildUserProfile_cat(steamid64: str, TopGames_n: int = 50) -> Counter:
+    """
+    Another preference profile based on categories.
+    """
+    rows = all_fetch(
+        """
+        SELECT appid, pt_forever_min
+        FROM owned_games
+        WHERE steamid64 = ?
+        ORDER BY pt_forever_min DESC
+        LIMIT ?
+        """,
+        (steamid64, TopGames_n)
+    )
+
+    profile = Counter()
+
+    for r in rows:
+        appid = int(r["appid"])
+        mins = int(r["pt_forever_min"])
+        weight = math.log1p(mins)
+
+        details = await f_appdetails_cached(appid)
+        if not details:
+            continue
+
+        cat = details.get("categories", [])
+        catNames = [c.get("description") for c in cat if c.get("description")]
+
+        for c in catNames:
+            profile[c] += weight
+
+        return profile
+
 async def GameScoring_genre(appid: int, user_profile: Counter) -> tuple[float, list[str]]:
     """
     Sums up weights for each genre; 
@@ -72,20 +107,28 @@ async def GameScoring_genre(appid: int, user_profile: Counter) -> tuple[float, l
 def TopProfileGenres_get(profile, i = 3):
     return [gen for gen, _ in profile.most_common(i)]
 
-def GenCandidates(profile, limit = 300):
+def GenCandidates(profile, limit = 300, explore = 150): # >>> explore -> random sample; should add some diversity <<<
     TopGenres = TopProfileGenres_get(profile)
 
     if not TopGenres:
         return []
 
     rows = all_fetch("SELECT appid, genres FROM app_index")
-    candidates = []
+    MatchedGenres, OtherGenres = [], []
 
     for r in rows:
         appid = int(r["appid"])
-        genres = json.loads(r["genres"])
+        genres = json.loads(r["genres"] or "[]")
 
-        if any(gen in genres for gen in TopGenres):
-            candidates.append(appid)
+        if TopGenres and any(gen in genres for gen in TopGenres):
+            MatchedGenres.append((appid))
+        else:
+            OtherGenres.append((appid))
 
+    if len(OtherGenres) > explore :
+        OtherGenres = random.sample(OtherGenres, explore)
+
+    candidates = MatchedGenres + OtherGenres
+    random.shuffle(candidates)
+    
     return candidates[:limit]
